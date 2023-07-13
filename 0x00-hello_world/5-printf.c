abc39fb2b0aba61ae66baba7de10fafe1188dae4
phemosky <phemosky2002@gmail.com>
2023-07-13 15:59:46 -0700

    -printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+*main - writing a C-programing that prints a line with printf function
+* Return: 0 (Success)
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
