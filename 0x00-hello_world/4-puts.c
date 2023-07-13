0528a28f1bf4bc306b886f702835048a533107d3
phemosky <phemosky2002@gmail.com>
2023-07-13 15:42:11 -0700

    C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - writing a C-programing that prints a line with puts function
  *  Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
