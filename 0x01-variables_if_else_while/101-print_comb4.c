f08e901802481690c1bd0ce0c75c0463e8e62d03
phemosky <phemosky2002@gmail.com>
2023-07-17 10:25:25 -0700

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,28 +6,28 @@
 
 int main(void)
 {
-	int a, b, c;
+int a, b, c;
 
-	for (a = 48; a < 58; a++)
-	{
-		for (b = 49; b < 58; b++)
-		{
-			for (c = 50; c < 58; c++)
-				{
-				if (c > b && b > a)
-				{
-					putchar(a);
-					putchar(b);
-					putchar(c);
-				if (a != 55 || b != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+for (a = 48; a < 58; a++)
+{
+for (b = 49; b < 58; b++)
+{
+for (c = 50; c < 58; c++)
+{
+if (c > b && b > a)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a != 55 || b != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
