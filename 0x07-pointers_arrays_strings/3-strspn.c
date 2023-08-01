660fc211946799ec993a46c3154275a84093db77
phemosky <phemosky2002@gmail.com>
2023-08-01 09:45:58 -0700

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int u = 0;
+	int v;
+
+	while (*s)
+	{
+		for (v = 0; accept[v]; v++)
+		{
+			if (*s == accept[v])
+			{
+				u++;
+				break;
+			}
+			else if (accept[v + 1] == '\0')
+				return (u);
+		}
+		s++;
+	}
+	return (u);
+}
