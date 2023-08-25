8084ad813eedb08a114eec4270dd04c91129c456
Oluwafemi Ogunleye <phemosky2002@gmail.com>
2023-08-25 12:26:12 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Returns Numbers of Elements Linked in a List
+ * @h: Pointer to List of list_t 
+ * Return: number of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t z = 0;
+
+	while (h)
+	{
+		z++;
+		h = h->next;
+	}
+	return (z);
+}
